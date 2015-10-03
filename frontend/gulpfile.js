356394dfaa42351d8cc9f25e4f19a77ad47729d0
Kevin Gosse <gossekevin1@gmail.com>
2015-10-03 13:53:13 +0200

    initial commit

---
+++ frontend/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('default', function(){
+	gulp.src('app/index.html')
+	.pipe(gulp.dest('temp'));
+
+	gulp.src('temp')
+	.pipe(webserver({
+		open: true
+		}));
+});
