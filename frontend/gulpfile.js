513574e9cbbfc4fdf7fe346a05b78be0cb7c001f
Kevin Gosse <gossekevin1@gmail.com>
2015-10-03 16:37:02 +0200

    autoload our custom scripts

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var mainBowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
 
 var paths = {
-  temp:  'temp',
-  index: 'app/index.html'
+  temp:       'temp',
+  tempVendor: 'temp/vendor',
+  index:      'app/index.html'
 };
 
-gulp.task('default', ['tempSetup', 'serve']);
+gulp.task('default', ['scripts', 'serve']);
 
-gulp.task('tempSetup', function(){
-  gulp.src(paths.index)
+gulp.task('scripts', function(){
+  var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
+
+  var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+
+  tempIndex.pipe(inject(tempVendors, {relative: true}))
     .pipe(gulp.dest(paths.temp));
 });
 
