d6f84b21d0ada27857420b5536e0158a103b92e5
Kevin Gosse <gossekevin1@gmail.com>
2015-10-03 17:33:32 +0200

    autoload our custom scripts

---
+++ frontend/app/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by kevin gosse on 03/10/2015.
+ */
+
+angular.module('app', []);
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -14,15 +14,22 @@ gulp.task('default', ['scripts', 'serve']);
 gulp.task('scripts', function(){
   var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
 
+  var scripts = gulp.src('app/**/*.js').pipe(gulp.dest(paths.temp));
+
   var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
 
-  tempIndex.pipe(inject(tempVendors, {relative: true}))
+  tempIndex
+    .pipe(inject(scripts, {relative: true}))
+    .pipe(inject(tempVendors, {
+      relative: true,
+      name:     'vendorInject'
+    }))
     .pipe(gulp.dest(paths.temp));
 });
 
 gulp.task('serve', function(){
   gulp.src(paths.temp)
     .pipe(webserver({
-      open: true
+      //open: true
     }));
 });
