4f2dada8f7582d26350e5ff18db357681f271afe
Kevin Gosse <gossekevin1@gmail.com>
2015-11-05 22:25:25 +0100

    route with ui-router and add an new post state

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,10 +2,20 @@
  * Created by kevin gosse on 03/10/2015.
  */
 
-angular.module('app', ['satellizer', 'ui.bootstrap'])
-  .config(function($authProvider){
+angular.module('app', ['satellizer', 'ui.bootstrap', 'ui.router'])
+  .config(function($authProvider, $stateProvider){
     $authProvider.twitter({
       url: '/api/user/login',
       authorizationEndpoint: 'https://api.twitter.com/oauth/authorize'
+    });
+
+    $stateProvider.state('posts', {
+      url: '/',
+      templateUrl: 'myposts.html',
+      controller: 'MyPosts'
+    }).state('post', {
+      url: '/post',
+      templateUrl: 'post.html',
+      controller: 'Post'
     })
   });
